1105756a57c43eb41481c6f63919abad44319b71
Jonathan A. Bennett <doulos05@gmail.com>
2023-07-03 15:55:30 +0900

    Clean up dead code and add comments

--- board.lisp
+++ board.lisp
@@ -17,26 +17,22 @@
   ((tile-hash
     :initarg :tiles
     :accessor tiles
+    :initform (make-hash-table :test 'equalp)
     :documentation "The hash of the Tile objects which make up the map, stored by xy coordinates.")
    (units
-    :initarg units
+    :initarg :units
     :accessor units
+    :initform '()
     :documentation "A hash of the Unit objects on the map, stored by xy coordinates.")))
 
 (defstruct tile
-  elevation terrain depth)
+  hexagon elevation terrain depth)
 
-;; Code to convert from qrs to xy and back again.
-;; Note that &1 is bitwise and, how do you do that in Common Lisp?
-;; function axial-to-oddq(hex):
-;;   var col = hex.q
-;;   var row = hex.r + (hex.q - (hex.q&1))/2
-;;   return point(col, rol)
-
-;; function oddq-to-axial(hex):
-;;   var q = hex.col
-;;   var r = hex.row - (hex.col - (hex.col&1)) / 2
-;;   return Hex(q, r)
+(defmethod insert-tile ((g grid) (new-tile tile))
+  "Insert hexagon `h' into the `tile-hash' of grid `g'."
+  (if (gethash (offset-from-hex (hexagon new-tile)) (tiles g))
+      (gethash (offset-from-hex (hexagon new-tile)) (tiles g))
+      (setq (gethash (offset-from-hex (hexagon new-tile)) (tiles g)) new-tile)))
 
 ;; Begin by defining a Hexagon class. We are using the Cubic constructor and
 ;; storage method described by Amin at Red Blob Games.
@@ -62,6 +58,8 @@
                      :r r
                      :s s)))
 
+
+;; Code to convert from qrs to xy and back again.
 (defun hex-from-offset (&key col row)
   "Creates hexes from the .board files used by MegaMek, which use an offset coordinate system instead of cubic like I do."
   (let ((q col)
@@ -70,6 +68,7 @@
   (make-hexagon :q q :r r :s s))
 
 (defmethod offset-from-hex ((hex hexagon))
+  "Creates xy coordinates from cubic coordinates using a hexagon."
   (let ((row (+ (hex r) (floor (/ (+ (hex q) (* (bit-and (hex q)) -1))))))
         (col (hex q)))
     (list col row)))
@@ -93,23 +92,18 @@
                 :s (- (s a) (s b))))
 
 (defmethod hex-multiply ((a hexagon) x)
-  "Uses Cartesian multiplication to multiply two hexagons together."
+  "Uses Cartesian multiplication to multiply a hex by a value x together."
   (make-hexagon :q (* (q a) x)
                 :r (* (r a) x)
                 :s (* (s a) x)))
 
-(defun hex-length (hex)
+(defmethod hex-distance ((a hexagon) (b hexagon))
   "The length of the distance between two hexagons is calculated similarly to
 Manhattan distances with a square grid, but you half the sum to get the final
-distance. The function cannot be called without hex-distance to produce the
-appropriate hexagon first."
-  (/ (+ (abs (q hex)) (abs (r hex)) (abs (s hex))) 2))
+distance."
+  (let ((hex-length (hex-subtract a b)))
+    (/ (+ (abs (q hex)) (abs (r hex)) (abs (s hex))) 2)))
 
-(defmethod hex-distance ((a hexagon) (b hexagon))
-  "This produces the hex we use in hex-length."
-  (hex-length (hex-subtract a b)))
-
-;; We use the following Vector to calculate neighbors.
 (defvar *hex-directions* (vector (make-hexagon :q 1 :r 0 :s -1)
                                  (make-hexagon :q 1 :r -1 :s 0)
                                  (make-hexagon :q 0 :r -1 :s 1)
@@ -119,14 +113,16 @@ appropriate hexagon first."
   "This vector describes the offsets to calculate neighbors.")
 
 (defun hex-direction (direction)
+  "Returns the hex in a given direction."
   (if (and (<= 0 direction) (> 6 direction))
       (elt *hex-directions* direction)))
 
 (defmethod hex-neighbor ((hex hexagon) direction)
+  "Finds the neighbor of a `hex' in a given `direction'.
+`direction' must be an integer between 0 and 5 inclusive."
   (hex-addition hex (hex-direction direction)))
 
 ;; Display Implementation
-
 (defstruct layout
   hex-to-pixel-matrix
   pixel-to-hex-matrix
@@ -136,10 +132,8 @@ appropriate hexagon first."
   x-origin
   y-origin)
 
-;; This function does the math to convert from a q,r,s address to an x,y
-;; position for the center fo the hex.
-
 (defun hex-to-pixel (hex layout)
+  "Converts from a q,r,s address, to an x,y pixel position for the center of the hex."
   (let ((vec (layout-hex-to-pixel-matrix layout)))
     (make-point (+ (* (+ (* (elt vec 0) (q hex))
                             (* (elt vec 1) (r hex)))
@@ -151,6 +145,8 @@ appropriate hexagon first."
                       (layout-y-origin layout)))))
 
 (defun pixel-to-hex (mouse-point layout)
+  "Converts from an x,y pixel address to a q,r,s hex address. Called to determine which
+hex is being clicked on / hovered over."
   (let* ((vec (layout-pixel-to-hex-matrix layout))
          (modified-point (make-point (/ (- (point-x mouse-point) (layout-x-origin layout)) (layout-x-size layout))
                                      (/ (- (point-y mouse-point) (layout-y-origin layout)) (layout-y-size layout))))
@@ -159,6 +155,7 @@ appropriate hexagon first."
     (make-hexagon :q calc-q :r calc-r :s (+ (* calc-q -1) (* calc-r -1)))))
 
 (defun find-hex-corner (center corner layout)
+  "Find the x,y pixel coordinates for the corner of a hex."
   (let ((angle (* 2.0 pi (/ (+ (layout-start-angle layout)
                                corner) 6))))
     (make-point (+ (* (layout-x-size layout)
@@ -167,9 +164,6 @@ appropriate hexagon first."
                 (+ (* (layout-y-size layout)
                          (sin angle))
                       (point-y center)))))
-
-(defun point-to-list (point)
-  (list (point-x point) (point-y point)))
 
 (defun draw-hex (hex layout)
   (let ((center (hex-to-pixel hex layout))
