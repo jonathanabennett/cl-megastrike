ec6487fbaa99d043f0e42927115b3d2f96962460
Jonathan A. Bennett <doulos05@gmail.com>
2023-07-04 16:59:37 +0900

    Replace hexagon CLOS class with a struct

--- board.lisp
+++ board.lisp
@@ -1,11 +1,19 @@
 (in-package #:alphastrike)
 
 (defun parse-hex-address (str)
+"A hex address from a board file is a string in the format xxyy"
   (let ((x (parse-integer (subseq str 0 2)))
         (y (parse-integer (subseq str 2 4))))
     (list x y)))
 
 (defun parse-hex-line (line)
+  "A line from a .board file looks like this:
+
+hex xxyy elevation terrain style
+
+For example:
+
+hex 0101 3 \"woods:1;foliage_elev:2\" \"grass\""
   (let* ((line-string (cl-ppcre:split "( )" line))
          (hex-address (nth 1 line-string))
          (elevation   (nth 2 line-string))
@@ -13,103 +21,88 @@
          (style       (nth 4 line-string)))
     (list hex-address elevation terrain style)))
 
-(defclass grid ()
-  ((tile-hash
-    :initarg :tiles
-    :accessor tiles
-    :initform (make-hash-table :test 'equalp)
-    :documentation "The hash of the Tile objects which make up the map, stored by xy coordinates.")
-   (units
-    :initarg :units
-    :accessor units
-    :initform '()
-    :documentation "A hash of the Unit objects on the map, stored by xy coordinates.")))
+;; (defclass grid ()
+;;   ((tile-hash
+;;     :initarg :tiles
+;;     :accessor tiles
+;;     :initform (make-hash-table :test 'equalp)
+;;     :documentation "The hash of the Tile objects which make up the map, stored by xy coordinates.")
+;;    (units
+;;     :initarg :units
+;;     :accessor units
+;;     :initform '()
+;;     :documentation "A hash of the Unit objects on the map, stored by xy coordinates.")))
 
-(defstruct tile
-  hexagon elevation terrain depth)
+;; (defstruct tile
+;;   hexagon elevation terrain depth)
 
-(defmethod insert-tile ((g grid) (new-tile tile))
-  "Insert hexagon `h' into the `tile-hash' of grid `g'."
-  (if (gethash (offset-from-hex (hexagon new-tile)) (tiles g))
-      (gethash (offset-from-hex (hexagon new-tile)) (tiles g))
-      (setq (gethash (offset-from-hex (hexagon new-tile)) (tiles g)) new-tile)))
+;; (defmethod insert-tile ((g grid) (new-tile tile))
+;;   "Insert hexagon `h' into the `tile-hash' of grid `g'."
+;;   (if (gethash (offset-from-hex (tile-hexagon new-tile)) (tiles g))
+;;       (gethash (offset-from-hex (tile-hexagon new-tile)) (tiles g))
+;;       (setq (gethash (offset-from-hex (tile-hexagon new-tile)) (tiles g)) new-tile)))
 
 ;; Begin by defining a Hexagon class. We are using the Cubic constructor and
 ;; storage method described by Amin at Red Blob Games.
 
-(defclass hexagon ()
-  ((q
-    :initarg :q
-    :accessor q
-    :documentation "location on the Q axis.")
-   (r
-    :initarg :r
-    :accessor r
-    :documentation "location on the R axis.")
-   (s
-    :initarg :s
-    :accessor s
-    :documentation "location on the S axis.")))
+(defstruct hexagon ()
+  q r s)
 
-(defun make-hexagon (&key q r s)
+(defun new-hexagon (&key q r s)
   (if (eq (+ q r s) 0)
-      (make-instance 'hexagon
-                     :q q
-                     :r r
-                     :s s)))
-
+      (make-hexagon :q q :r r :s s)))
 
 ;; Code to convert from qrs to xy and back again.
 (defun hex-from-offset (&key col row)
   "Creates hexes from the .board files used by MegaMek, which use an offset coordinate system instead of cubic like I do."
   (let ((q col)
-        (- row (floor (/ (+ col (* (bit-and col) -1)) 2)))
-        (s (+ (* q -1) r))))
-  (make-hexagon :q q :r r :s s))
+        (r (- row (floor (/ (+ col (* (bit-and col) -1)) 2))))
+        (s (+ (* q -1) r)))
+    (new-hexagon :q q :r r :s s)))
 
-(defmethod offset-from-hex ((hex hexagon))
+(defun offset-from-hex (hex)
   "Creates xy coordinates from cubic coordinates using a hexagon."
-  (let ((row (+ (hex r) (floor (/ (+ (hex q) (* (bit-and (hex q)) -1))))))
-        (col (hex q)))
+  (let ((row (+ (hexagon-r hex) (floor (/ (+ (hexagon-q q) (* (bit-and (hexagon-q q)) -1))))))
+        (col (hexagon-q q)))
     (list col row)))
 
-(defmethod same-hex ((hex1 hexagon) (hex2 hexagon))
+(defun same-hex (hex1 hex2)
   "Hexagons are equal if their q, r, and s coordinates are the same."
-  (and (= (s hex1) (s hex2))
-       (= (r hex1) (r hex2))
-       (= (q hex1) (q hex2))))
+  (and (= (hexagon-s hex1) (hexagon-s hex2))
+       (= (hexagon-r hex1) (hexagon-r hex2))
+       (= (hexagon-q hex1) (hexagon-q hex2))))
 
-(defmethod hex-addition ((a hexagon) (b hexagon))
+(defun hex-addition (hex1 hex2)
   "Uses Cartesian addition to add two hexagons together."
-  (make-hexagon :q (+ (q a) (q b))
-                :r (+ (r a) (r b))
-                :s (+ (s a) (s b))))
+  (new-hexagon :q (+ (hexagon-q hex1) (hexagon-q hex2))
+                :r (+ (hexagon-r hex1) (hexagon-r hex2))
+                :s (+ (hexagon-s hex1) (hexagon-s hex2))))
 
-(defmethod hex-subtract ((a hexagon) (b hexagon))
+(defun hex-subtract (hex1 hex2)
   "Uses Cartesian subtraction to subtract hexagon b from hexagon a."
-  (make-hexagon :q (- (q a) (q b))
-                :r (- (r a) (r b))
-                :s (- (s a) (s b))))
+  (new-hexagon :q (- (hexagon-q a) (hexagon-q b))
+                :r (- (hexagon-r a) (hexagon-r b))
+                :s (- (hexagon-s a) (hexagon-s b))))
 
-(defmethod hex-multiply ((a hexagon) x)
+(defun hex-multiply (hex x)
   "Uses Cartesian multiplication to multiply a hex by a value x together."
-  (make-hexagon :q (* (q a) x)
-                :r (* (r a) x)
-                :s (* (s a) x)))
+  (new-hexagon :q (* (hexagon-q hex) x)
+                :r (* (hexagon-r hex) x)
+                :s (* (hexagon-s hex) x)))
 
-(defmethod hex-distance ((a hexagon) (b hexagon))
+(defun hex-distance (hex1 hex2)
   "The length of the distance between two hexagons is calculated similarly to
 Manhattan distances with a square grid, but you half the sum to get the final
 distance."
-  (let ((hex-length (hex-subtract a b)))
-    (/ (+ (abs (q hex)) (abs (r hex)) (abs (s hex))) 2)))
+  (let ((hex-length (hex-subtract hex1 hex2)))
+    (/ (+ (abs (hexagon-q hex)) (abs (hexagon-r hex)) (abs (hexagon-s hex))) 2)))
 
-(defvar *hex-directions* (vector (make-hexagon :q 1 :r 0 :s -1)
-                                 (make-hexagon :q 1 :r -1 :s 0)
-                                 (make-hexagon :q 0 :r -1 :s 1)
-                                 (make-hexagon :q -1 :r 0 :s 1)
-                                 (make-hexagon :q -1 :r 1 :s 0)
-                                 (make-hexagon :q 0 :r 1 :s -1))
+(defvar *hex-directions* (vector (new-hexagon :q 1 :r 0 :s -1)
+                                 (new-hexagon :q 1 :r -1 :s 0)
+                                 (new-hexagon :q 0 :r -1 :s 1)
+                                 (new-hexagon :q -1 :r 0 :s 1)
+                                 (new-hexagon :q -1 :r 1 :s 0)
+                                 (new-hexagon :q 0 :r 1 :s -1))
   "This vector describes the offsets to calculate neighbors.")
 
 (defun hex-direction (direction)
@@ -117,7 +110,7 @@ distance."
   (if (and (<= 0 direction) (> 6 direction))
       (elt *hex-directions* direction)))
 
-(defmethod hex-neighbor ((hex hexagon) direction)
+(defun hex-neighbor (hex direction)
   "Finds the neighbor of a `hex' in a given `direction'.
 `direction' must be an integer between 0 and 5 inclusive."
   (hex-addition hex (hex-direction direction)))
@@ -135,12 +128,12 @@ distance."
 (defun hex-to-pixel (hex layout)
   "Converts from a q,r,s address, to an x,y pixel position for the center of the hex."
   (let ((vec (layout-hex-to-pixel-matrix layout)))
-    (make-point (+ (* (+ (* (elt vec 0) (q hex))
-                            (* (elt vec 1) (r hex)))
+    (make-point (+ (* (+ (* (elt vec 0) (hexagon-q hex))
+                            (* (elt vec 1) (hexagon-r hex)))
                          (layout-x-size layout))
                       (layout-x-origin layout))
-                (+ (* (+ (* (elt vec 2) (q hex))
-                            (* (elt vec 3) (r hex)))
+                (+ (* (+ (* (elt vec 2) (hexagon-q hex))
+                            (* (elt vec 3) (hexagon-r hex)))
                          (layout-y-size layout))
                       (layout-y-origin layout)))))
 
