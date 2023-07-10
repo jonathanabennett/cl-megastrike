ba522851043eb9af7e4d08b329c1d493cf7aa149
Jonathan A. Bennett <doulos05@gmail.com>
2023-07-10 16:32:29 +0900

    Formatting for eventual exports

--- package.lisp
+++ package.lisp
@@ -1,7 +1,8 @@
 ;;;; package.lisp
 
 (defpackage #:alphastrike
-  (:use #:clim #:clim-lisp))
+  (:use #:clim #:clim-lisp)
+  )
 
 (defgeneric display (obj)
   (:documentation "Pretty print an object for display in the GUI"))
