5f55ed1b66f774653c065a45d309f0a1be110db8
Jonathan A. Bennett <doulos05@gmail.com>
2023-07-14 10:07:49 +0900

    Add button controls

--- alphastrike.lisp
+++ alphastrike.lisp
@@ -25,7 +25,10 @@
       (if (string= (pathname-type file) "lisp") (load file)))))
 
 (define-application-frame alphastrike ()
-  ()
+  ((active-unit
+    :initform nil
+    :accessor active-unit))
+  (:menu-bar nil)
   (:panes
      (world
       :application
@@ -34,7 +37,9 @@
      (record-sheet
       :application
       :display-function #'display-element)
-     (int :interactor))
+     (menu :command-menu)
+     ;;(int :interactor)
+     )
   (:layouts
    (:default
     (vertically ()
@@ -42,7 +47,7 @@
        (horizontally ()
          (:fill world)
          (1/4 record-sheet)))
-      (1/10 int)))))
+      (1/10 (vertically () menu))))))
 
 (defmethod display-map ((frame alphastrike) stream)
   (maphash (lambda (k v)
@@ -69,13 +74,13 @@
            :prompt "Amount: "))
   (take-damage target damage))
 
-(define-alphastrike-command (com-reset :name "Reset")
+(define-alphastrike-command (com-reset :name "Reset" :menu t)
   ((target 'combat-unit
            :prompt "Target: "))
   (setf (damageable/cur-armor target) (damageable/max-armor target))
   (setf (damageable/cur-struct target) (damageable/max-struct target)))
 
-(define-alphastrike-command (com-measure-range :name "Measure Range")
+(define-alphastrike-command (com-measure-range :name "Measure Range" :menu t)
   ((origin 'combat-unit)
    (target 'tile))
   (notify-user *application-frame*
@@ -84,28 +89,38 @@
                                                                   :s (location/s origin))
                                                      (tile-hexagon target)))))
 
-(define-alphastrike-command (com-command-move-unit :name "Move")
+(define-alphastrike-command (com-command-move-unit :name "Move" :menu t)
   ((unit 'combat-unit)
    (destination 'tile))
   (move-unit unit destination))
 
-(define-alphastrike-command (com-command-attack :name "Attack")
+(define-alphastrike-command (com-command-attack :name "Attack" :menu t)
   ((attacker 'combat-unit)
    (target 'combat-unit))
   (make-attack attacker target))
 
-(define-alphastrike-command (com-roll :name "Roll")
+(define-alphastrike-command (com-roll :name "Roll" :menu t)
   ()
   (notify-user *application-frame* (format nil "Rolled a ~d on 2D6." (roll2d))))
+
+(define-alphastrike-command (com-select-unit :name "Select")
+  ((selected 'combat-unit))
+  (setf (active-unit *application-frame*) selected))
+
+(define-alphastrike-command (com-quit-game :name "Quit Game" :menu t)
+  ()
+  (frame-exit *application-frame*))
+
 ;; (define-presentation-to-command-translator tile-selector
 ;;     (tile com-inspect-tile alphastrike :gesture :select)
 ;;     (object)
 ;;   (list object))
 
-;; (define-presentation-to-command-translator unit-selector
-;;     (combat-unit com-select-combat-unit alphastrike :gesture :select)
-;;     (object)
-;;   (list object))
+(define-presentation-to-command-translator unit-selector
+    (combat-unit com-select-unit alphastrike :gesture :select)
+    (object)
+  (list object))
+
 
 (defun main ()
   ;;(load-data)
