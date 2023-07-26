f0e96d6379350d26cfbb54116b02d300abd00abc
Jonathan A. Bennett <doulos05@gmail.com>
2023-07-26 22:20:00 +0900

    Units required to play Sword and Dragon
    Adds the 7 units found in the Sword and the Dragon scenario.

---
+++ data/units/dragon-drg-1c.lisp
@@ -0,0 +1,21 @@
+(in-package :megastrike)
+
+(add-or-update-mek
+ :short-name "DRG-1C"
+ :long-name "Dragon DRG-1C"
+ :unit-type "BM"
+ :role "Skirmish"
+ :pv 35
+ :size 3
+ :armor 7
+ :structure 5
+ :mv-string "5"
+ :short 1
+ :medium 2
+ :long 1 ;; Enter 0.5 for 0*
+ :ov 0
+ :specials "SRCH SOA IF1 REAR1/1/-"
+ :display "data/images/units/mechs/Dragon_1C.png"
+ :tro
+""
+)
