aed91a59d169e007deb6c360256b580c3c9f0f28
Jonathan Bennett <jonathanbennett@boromir.local>
2023-10-10 17:53:48 +0900

    Add tests to the Hexagon class

--- test/main.lisp
+++ test/main.lisp
@@ -5,7 +5,7 @@
 
 (def-suite* test-megastrike :description "Master test suite for Megastrike.")
 
-(defun test-megastrike ()
+(defun all-tests ()
   (run! 'test-megastrike))
 
 (test mechset-parser-tests
@@ -26,3 +26,53 @@
                                             (<= result 6))))))
 (test dice-rolling
   (is-true (test-a-lot-of-rolls)))
+
+(def-suite* test-hexagons :description "Testing the hexagon API."
+                         :in test-megastrike)
+
+(test hexagon-creation-tests
+  (is (typep (megastrike::new-hexagon :q 0 :r 0 :s 0) 'megastrike::hexagon)
+      "Failed to create a hexagon when given correct coordinates."))
+
+(test hexagon-equality
+  (let ((hex1 (megastrike::new-hexagon :q 0 :r 0 :s 0))
+        (matching-hex (megastrike::new-hexagon :q 0 :r 0 :s 0))
+        (mismatched-hex (megastrike::new-hexagon :q 0 :r 1 :s -1)))
+    (is-true (megastrike::same-hex hex1 hex1)
+             "A hex object should match itself.")
+    (is-true (megastrike::same-hex hex1 matching-hex)
+             "A hex object should match a hex that has the same q,r,s address.")
+    (is-false (megastrike::same-hex hex1 mismatched-hex)
+              "A hex object should NOT match a hex that has a different q,r,s address")
+    (is-false (megastrike::same-hex "" hex1)
+              "Disparate types are not the same.")
+    (is-false (megastrike::same-hex hex1 "")
+              "Disparate types are not the same.")))
+
+(test hexagonal-addition
+  (let ((base-hex (megastrike::new-hexagon :q 0 :r 0 :s 0))
+        (diff-q-hex (megastrike::new-hexagon :q 1 :r 0 :s -1))
+        (diff-r-hex (megastrike::new-hexagon :q 0 :r 1 :s -1))
+        (diff-s-hex (megastrike::new-hexagon :q -1 :r -1 :s 2)))
+    (is (megastrike::same-hex (megastrike::hex-addition base-hex diff-q-hex)
+                              (megastrike::new-hexagon :q 1 :r 0 :s -1)))
+    (is (megastrike::same-hex (megastrike::hex-addition base-hex diff-r-hex)
+                              (megastrike::new-hexagon :q 0 :r 1 :s -1)))
+    (is (megastrike::same-hex (megastrike::hex-addition base-hex diff-s-hex)
+                              (megastrike::new-hexagon :q -1 :r -1 :s 2)))
+    (is (megastrike::same-hex (megastrike::hex-addition diff-q-hex diff-r-hex)
+                              (megastrike::new-hexagon :q 1 :r 1 :s -2)))))
+
+(test hexagonal-subtraction
+  (let ((base-hex (megastrike::new-hexagon :q 0 :r 0 :s 0))
+        (diff-q-hex (megastrike::new-hexagon :q 1 :r 0 :s -1))
+        (diff-r-hex (megastrike::new-hexagon :q 0 :r 1 :s -1))
+        (diff-s-hex (megastrike::new-hexagon :q -1 :r -1 :s 2)))
+    (is (megastrike::same-hex (megastrike::hex-subtract base-hex diff-q-hex)
+                              (megastrike::new-hexagon :q -1 :r 0 :s 1)))
+    (is (megastrike::same-hex (megastrike::hex-subtract base-hex diff-r-hex)
+                              (megastrike::new-hexagon :q 0 :r -1 :s 1)))
+    (is (megastrike::same-hex (megastrike::hex-subtract base-hex diff-s-hex)
+                              (megastrike::new-hexagon :q 1 :r 1 :s -2)))
+    (is (megastrike::same-hex (megastrike::hex-subtract diff-q-hex diff-r-hex)
+                              (megastrike::new-hexagon :q 1 :r -1 :s -2)))))
