a8a4a05a819159bc621e41f7aad6c9ec6f0f9704
Jonathan A. Bennett <doulos05@gmail.com>
2023-11-07 17:08:56 +0900

    Correct install.sh setup

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
 # This script assumes you have installed the following list of prerequisites.
 # - GTK4.10 or newer and its dependencies.
 # - Roswell Lisp Manager and its dependencies.
 # See your distro's package manager to install them. For MacOS, use Homebrew.
-#
+# 
 # It will run on MacOS and Linux. For Windows, this should also run on WSL2
 # or MSYS2, provided you have installed the prerequisites.
 
